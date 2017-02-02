9deffd7bf31209e63b6edf4a1f6085797ec761dc
nulluuuuuu <luye@email.arizona.edu>
2017-02-02 13:49:18 -0700

    added lab 3

---
+++ Lab3/lab3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>All work</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php
+	for($i = 0; $i< 200; ++$i){
+		print("<p>All work and no play makes jack a dull boy</p>")
+	}
+?>
+</body>
+</html>
